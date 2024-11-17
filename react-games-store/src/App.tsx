2e2ee57ae005b3a333b6cc843851ab38778c8759
possionthefish1 <92135698+possionthefish1@users.noreply.github.com>
2024-11-17 12:01:48 +0700

    dark mode work, b4 tailwind

--- react-games-store/src/App.tsx
+++ react-games-store/src/App.tsx
@@ -1,61 +1,7 @@
-import { useState } from 'react';
-import viteLogo from '/vite.svg';
-import reactLogo from './assets/react.svg';
 import './App.css';
-import { Button } from '@/components/ui/button';
-import { HStack } from '@chakra-ui/react';
 
 function App() {
-	const [count, setCount] = useState<number>(0);
-
-	return (
-		<>
-			<div>
-				<HStack>
-					<Button>Click me</Button>
-					<Button>Click me</Button>
-				</HStack>
-				<a
-					href='https://vite.dev'
-					target='_blank'
-					rel='noreferrer'>
-					<picture>
-						<img
-							src={viteLogo}
-							className='logo'
-							alt='Vite logo'
-						/>
-					</picture>
-				</a>
-				<a
-					href='https://react.dev'
-					target='_blank'
-					rel='noreferrer'>
-					<picture>
-						<img
-							src={reactLogo}
-							className='logo react'
-							alt='React logo'
-						/>
-					</picture>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button
-					type='button'
-					onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	);
+	return <h1>Home Page</h1>;
 }
 
 export default App;
--- react-games-store/src/main.tsx
+++ react-games-store/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from '@/components/ui/provider.tsx';
-// import Smth from 'c'
 import './index.css';
 import App from './App.tsx';
 
